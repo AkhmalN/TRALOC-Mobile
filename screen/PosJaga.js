9479ea0483537c7e8f6ae7918df51f37f5c19144
AkhmalNovanda <cocwar427@gmail.com>
2023-10-22 10:24:20 +0700

    deleting polygon

--- screen/PosJaga.js
+++ screen/PosJaga.js
@@ -3,17 +3,17 @@ import { StyleSheet } from "react-native";
 import MapView, { Marker, Callout, Polygon } from "react-native-maps";
 
 function PosJaga() {
-  const regionCoordinates = [
-    { latitude: -6.2819591647304325, longitude: 106.8397758308623 },
-    { latitude: -6.281840554254593, longitude: 106.8387687801136 },
-    { latitude: -6.280365825800194, longitude: 106.83892612556114 },
-    { latitude: -6.279580888698144, longitude: 106.83934060832388 },
-    { latitude: -6.279683715427403, longitude: 106.83952227208735 },
-    { latitude: -6.279588782939833, longitude: 106.83959787879728 },
-    { latitude: -6.279678819740212, longitude: 106.83990833027214 },
-    { latitude: -6.27999033392509, longitude: 106.84001822855781 },
-    { latitude: -6.281918061495241, longitude: 106.8397765420201 },
-  ];
+  // const regionCoordinates = [
+  //   { latitude: -6.2819591647304325, longitude: 106.8397758308623 },
+  //   { latitude: -6.281840554254593, longitude: 106.8387687801136 },
+  //   { latitude: -6.280365825800194, longitude: 106.83892612556114 },
+  //   { latitude: -6.279580888698144, longitude: 106.83934060832388 },
+  //   { latitude: -6.279683715427403, longitude: 106.83952227208735 },
+  //   { latitude: -6.279588782939833, longitude: 106.83959787879728 },
+  //   { latitude: -6.279678819740212, longitude: 106.83990833027214 },
+  //   { latitude: -6.27999033392509, longitude: 106.84001822855781 },
+  //   { latitude: -6.281918061495241, longitude: 106.8397765420201 },
+  // ];
   return (
     <View style={styles.container}>
       <View style={styles.mapContainer}>
@@ -26,12 +26,12 @@ function PosJaga() {
             longitudeDelta: 0.001, // This sets the zoom level
           }}
         >
-          <Polygon
+          {/* <Polygon
             coordinates={regionCoordinates}
             fillColor="rgba(0, 255, 0, 0.02)" // Warna isi wilayah
             strokeColor="gray" // Warna garis pinggiran
             strokeWidth={2} // Lebar garis pinggiran
-          />
+          /> */}
           <Marker
             key={1}
             coordinate={{
